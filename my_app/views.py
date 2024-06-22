d259bd62b93888c05b3b0ff29248143551bc3b88
Belscki <136617733+Belscki@users.noreply.github.com>
2024-06-22 17:07:49 -0300

    Add files via upload

--- my_app/views.py
+++ my_app/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     content = """<div style='width:100%; height:100vh; display:flex;justify-content:center;align-items:center'>
+<<<<<<< HEAD
+                    <h1 style='font-size: 4rem; color:green'>BBBBBBBBBBBBBBBBBBBBBBBBBBBB</h1>
+=======
                     <h1 style='font-size: 4rem; color:green'>aAAAAAAAAAAAAAAAAAAAAAAA.</h1>
+>>>>>>> 5517210d6dc94bf88f124bc98137b67cd4b13f41
                 </div>"""
     return HttpResponse(content)
