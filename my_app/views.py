5517210d6dc94bf88f124bc98137b67cd4b13f41
Belscki <136617733+Belscki@users.noreply.github.com>
2024-06-22 17:02:59 -0300

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     content = """<div style='width:100%; height:100vh; display:flex;justify-content:center;align-items:center'>
-                    <h1 style='font-size: 4rem; color:green'>Voila! Welcome to Halogenius Ideas.</h1>
+                    <h1 style='font-size: 4rem; color:green'>aAAAAAAAAAAAAAAAAAAAAAAA.</h1>
                 </div>"""
     return HttpResponse(content)
