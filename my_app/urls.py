4d25fa86cdda263eb951f4122de5938229016398
Belscki <136617733+Belscki@users.noreply.github.com>
2024-06-23 00:28:50 -0300

    Update urls.py

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,5 +4,5 @@ from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home, name="home-view"),
-    path('home',home, name="home-view"),
+    path('home/',home, name="home-view"),
 ]
